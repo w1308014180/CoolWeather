43d6371e9116df3b756001139f8c94864ada8aa7
wkk <1308014180@qq.com>
2021-05-06 19:37:53 +0800

    加入创建数据库和表的各项配置

---
+++ app/src/main/java/com/example/coolweather/db/County.java
@@ -0,0 +1,42 @@
+package com.example.coolweather.db;
+
+import org.litepal.crud.LitePalSupport;
+
+public class County extends LitePalSupport {
+    private int id;
+    private String countyName;//县名
+    private String weatherId;//县的天气
+    private int cityId;//县所属的市的id
+
+    public int getId(){
+        return id;
+    }
+
+    public void setId(int id){
+        this.id = id;
+    }
+
+    public String getCountyName(){
+        return countyName;
+    }
+
+    public void setCountyName(String countyName){
+        this.countyName = countyName;
+    }
+
+    public String getWeatherId(){
+        return weatherId;
+    }
+
+    public void setWeatherId(String weatherId){
+        this.weatherId = weatherId;
+    }
+
+    public int getCityId(){
+        return cityId;
+    }
+
+    public void setCityId(int cityId){
+        this.cityId = cityId;
+    }
+}
